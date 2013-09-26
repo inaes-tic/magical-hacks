761c75bbbff2f38fe8a1f49497c73471582eae09
Niv Sardi <xaiki@evilgiggle.com>
2013-09-26 18:47:00 -0300

    link not copy
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- split-scenes-and-transcode.sh
+++ split-scenes-and-transcode.sh
@@ -12,7 +12,7 @@ for b in `cat scenes.txt`; do
                 I=`printf "fixed-%06d.png" $i`;
                 J=`printf   "$sc-%06d.png" $j`;
 
-                cp $I $sc/$J;
+                ln -sf ../$I $sc/$J;
                 j=$(($j+1))
         done ;
         echo "$FFMPEG -r ntsc -i '$sc-%06d.png' -c:v prores_ks -profile:v 3 -qscale:v 1 -vendor ap10 -pix_fmt yuv422p10le -vcodec prores $sc.mov" > $sc/render.sh
