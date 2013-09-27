cd04c5c90979c877274ef3985a35ed1ea2804cc2
Niv Sardi <xaiki@evilgiggle.com>
2013-09-26 21:42:08 -0300

    slurp + debug
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- cv-flow.pl
+++ cv-flow.pl
@@ -3,6 +3,8 @@
 use strict;
 
 use Getopt::Long;
+use File::Slurp; # libfile-slurp-perl
+use Data::Dumper;
 
 my $xf = './xaBuildFlow';
 my $si = './slowmoInterpolate';
@@ -18,15 +20,19 @@ my $args = "";
 my $aglob = "*.png";
 my $format = 'fixed-%06d.png';
 my $fops = '';
+my $fl;
+my $debug;
 
 GetOptions ("flowbuilder|xf|x=s"  => \$xf,
             "interpolator|si|i=s" => \$si,
             "first-frame|k=i"     => \$k,
+            "frame-list|l=s"      => \$fl,
             "fops|fo|o=s"         => \$fops,
             "skip|s=i"            => \$skip,
             "count|c=i"           => \$count,
             "method|m=s"          => \$method,
-            "dir|d=s"             => \$dir,
+#            "dir|d=s"             => \$dir,
+            "debug|d"             => \$debug,
             "format|fmt|f=s"      => \$format,
             "args|a=s"            => \$args,
             "glob|g=s"            => \$aglob,
@@ -39,6 +45,19 @@ my $o = 0;
 my @imgs = glob($aglob);
 my ($glob) = $aglob =~ m/([^\*]+$)/;
 
+sub godo (@) {
+    print @_;
+    if (! $debug) {
+        print `@_`;
+    }
+}
+
+my @frames;
+if ($fl) {
+    @frames = read_file($fl);
+#    print Dumper (@frames);
+}
+
 $format = "fixed-%06d$glob";
 
 sub img_add ($$$ ) {
@@ -46,7 +65,7 @@ sub img_add ($$$ ) {
     return sprintf ("$h%0".length($n)."d".$glob, $n + $o);
 }
 
-print "glob: $glob, imgs: @imgs\n";
+#print "glob: $glob, imgs: @imgs\n";
 foreach (@imgs) {
     if ($skip-- > 0) {
         print "skipping frame $_\n";
@@ -59,8 +78,20 @@ foreach (@imgs) {
     my $recf = sprintf ($format, $processed + $o);
     my $nexf = sprintf ($format, $processed + $o + 1);
 
+
+    if (@frames) {
+        if ($frames[0] > $n) {
+            print "got frame-list, skipping frame $_\n";
+            next;
+        } elsif ($frames[0] == $n) {
+            shift @frames;
+        } else {
+            die "something went wrong, got $frames[0] <  and $n\n";
+        }
+    }
+
     printf "%08d/$count: $_ -> $recf\n", $processed;
-    `cp $_ $recf`;
+    godo ("cp $_ $recf");
 
     unless (($n+$k)%5) {
         print "adding frame between: $_ and $next, to $nexf\n";
@@ -69,39 +100,38 @@ foreach (@imgs) {
             my $fflow = "flow-fwd-".($n)."-".($n+1).".sVflow";
             my $bflow = "flow-bkw-".($n+1)."-".($n).".sVflow";
             print "flow from $_ to $next\n";
-            print `$xf $_ $next $fflow`;
+            godo ("$xf $_ $next $fflow $fops");
             print "flow from $next to $_\n";
-            print `$xf $next $_ $bflow`;
+            godo ("$xf $next $_ $bflow");
             print "twoway interpolation\n";
-            print `$si twoway $_ $next $fflow $bflow .out%1$glob 0 2`;
+            godo ("$si twoway $_ $next $fflow $bflow .out%1$glob 0 2");
             print "saving center interpolation\n";
-            print `mv .out00000001$glob $nexf`;
-            print `rm -rf .out*`;
+            godo ("mv .out00000001$glob $nexf");
+            godo ("rm -rf .out*");
         } elsif ($method eq 'biflow') {
             my $fflow = "flow";
             my $bflow = "rflow";
             print "biflow from $_ to $next\n";
             print "$xf $_ $next $args\n";
-            print `$xf $_ $next $args`;
+            godo ("$xf $_ $next $args");
             print "twoway interpolation\n";
-            print "$si twoway $_ $next $fflow $bflow .out%1$glob 0 2\n";
-            print `$si twoway $_ $next $fflow $bflow .out%1$glob 0 2`;
+            godo ("$si twoway $_ $next $fflow $bflow .out%1$glob 0 2");
             print "saving center interpolation\n";
-            print `mv .out00000001$glob $nexf`;
-            print `rm -rf .out*`;
+            godo ("mv .out00000001$glob $nexf");
+            godo ("rm -rf .out*");
         } elsif ($method eq 'flowforward') {
             my $fflow = "flow-fwd-".($n)."-".($n+1).".sVflow";
             print "flow from $_ to $next\n";
-            print `$xf $_ $next $fflow`;
+            godo ("$xf $_ $next $fflow");
             print "oneway interpolation\n";
-            print `$si forward $_ $fflow .out%1$glob 0 2`;
+            godo ("$si forward $_ $fflow .out%1$glob 0 2");
             print "saving center interpolation\n";
-            print `mv .out00000001$glob $nexf`;
-            print `rm -rf .out*`;
+            godo ("mv .out00000001$glob $nexf");
+            godo ("rm -rf .out*");
 
         } elsif ($method eq 'average') {
             print "rendering average between $_ and $next\n";
-            print `convert $_ $next -average -flatten $nexf`;
+            godo ("convert $_ $next -average -flatten $nexf");
         } elsif ($method eq 'debug') {
             print "debug, skipping conversion\n";
         }
