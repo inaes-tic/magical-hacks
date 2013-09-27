9d7304ac148abaf030c21ee43607d3dc611fe2f3
Niv Sardi <xaiki@evilgiggle.com>
2013-09-27 13:27:11 -0300

    tmpdir
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- cv-flow.pl
+++ cv-flow.pl
@@ -4,10 +4,15 @@ use strict;
 
 use Getopt::Long;
 use File::Slurp; # libfile-slurp-perl
+use File::Temp qw/tempdir/;
+
+my $tmp = tempdir (CLEANUP => 1);
+
 use Data::Dumper;
 
 my $xf = './xaBuildFlow';
 my $si = './slowmoInterpolate';
+my $bf = './biflow.pl';
 my $k  = 0;
 my $skip = 0;
 my $count = -1;
@@ -19,15 +24,15 @@ my $processed = 0;
 my $args = "";
 my $aglob = "*.png";
 my $format = 'fixed-%06d.png';
-my $fops = '';
+my $follow = 0;
 my $fl;
 my $debug;
 
 GetOptions ("flowbuilder|xf|x=s"  => \$xf,
             "interpolator|si|i=s" => \$si,
+            "biflow|bf|b=s"       => \$bf,
             "first-frame|k=i"     => \$k,
             "frame-list|l=s"      => \$fl,
-            "fops|fo|o=s"         => \$fops,
             "skip|s=i"            => \$skip,
             "count|c=i"           => \$count,
             "method|m=s"          => \$method,
@@ -36,6 +41,7 @@ GetOptions ("flowbuilder|xf|x=s"  => \$xf,
             "format|fmt|f=s"      => \$format,
             "args|a=s"            => \$args,
             "glob|g=s"            => \$aglob,
+            "follow-links|w"      => \$follow,
     )
     or die("Error in command line arguments\n");
 
@@ -54,15 +60,20 @@ sub godo (@) {
 
 my @frames;
 if ($fl) {
-    @frames = read_file($fl);
+    @frames = read_file($fl, chomp => 1);
 #    print Dumper (@frames);
 }
 
-$format = "fixed-%06d$glob";
+#$format = $format || "fixed-%06d$glob";
 
 sub img_add ($$$ ) {
     my ($h, $n, $o) = @_;
     return sprintf ("$h%0".length($n)."d".$glob, $n + $o);
+}
+
+sub parse_img ($$ ) {
+    my ($img, $glob) = @_;
+    return $img =~ m/^(.+[^\d])(\d+)$glob/ or die "can't parse image: $img, $glob";
 }
 
 #print "glob: $glob, imgs: @imgs\n";
@@ -72,68 +83,82 @@ foreach (@imgs) {
         next;
     }
 
-    my ($h, $n) = $_ =~ m/^([^\d]+)(\d+)$glob/ or next;
+    my ($h, $n) = parse_img ($_, $glob);
     my $next = img_add ($h, $n, 1);
 
     my $recf = sprintf ($format, $processed + $o);
     my $nexf = sprintf ($format, $processed + $o + 1);
 
-
+    my $act = 0;
     if (@frames) {
-        if ($frames[0] > $n) {
-            print "got frame-list, skipping frame $_\n";
-            next;
-        } elsif ($frames[0] == $n) {
-            shift @frames;
-        } else {
-            die "something went wrong, got $frames[0] <  and $n\n";
+        my $img = $_;
+        if ($follow) {
+            $img = readlink || $_;
         }
+
+        my ($H, $N) = parse_img ($img, $glob);
+        print "\n==$n:$N==";
+        while ($frames[0] < $N) {
+            print "skipping $frames[0], we're far ahead\n";
+            shift @frames;
+        }
+        if ($frames[0] > $N) {
+            print "got frame-list, skipping frame $_ -> $img, next: '$frames[0]'\n";
+        } elsif ($frames[0] == $N) {
+            shift @frames;
+            print "processing\n";
+            $act++;
+        } else {
+            die "something went wrong, got $frames[0] <  and $N\n";
+        }
+    } else {
+        $act = (!($n+$k)%5);
     }
 
     printf "%08d/$count: $_ -> $recf\n", $processed;
-    godo ("cp $_ $recf");
+    godo ("ln -sf $_ $recf");
 
-    unless (($n+$k)%5) {
+    if ($act) {
         print "adding frame between: $_ and $next, to $nexf\n";
 
-        if ($method eq 'flow') {
+        if ($method eq 'flow' or $method eq 'a') {
             my $fflow = "flow-fwd-".($n)."-".($n+1).".sVflow";
             my $bflow = "flow-bkw-".($n+1)."-".($n).".sVflow";
             print "flow from $_ to $next\n";
-            godo ("$xf $_ $next $fflow $fops");
+            godo ("$xf $_ $next $fflow $args");
             print "flow from $next to $_\n";
             godo ("$xf $next $_ $bflow");
             print "twoway interpolation\n";
-            godo ("$si twoway $_ $next $fflow $bflow .out%1$glob 0 2");
+            godo ("$si twoway $_ $next $fflow $bflow $tmp/out%1$glob 0 0");
             print "saving center interpolation\n";
-            godo ("mv .out00000001$glob $nexf");
-            godo ("rm -rf .out*");
-        } elsif ($method eq 'biflow') {
+            godo ("mv $tmp/out0000000$glob $nexf");
+            godo ("rm -rf $tmp/*");
+        } elsif ($method eq 'biflow' or $method eq 'b') {
             my $fflow = "flow";
             my $bflow = "rflow";
             print "biflow from $_ to $next\n";
-            print "$xf $_ $next $args\n";
-            godo ("$xf $_ $next $args");
+            godo ("$bf $xf $_ $next $args");
             print "twoway interpolation\n";
-            godo ("$si twoway $_ $next $fflow $bflow .out%1$glob 0 2");
+            godo ("$si twoway $_ $next $fflow $bflow $tmp/%1$glob 0 0");
             print "saving center interpolation\n";
-            godo ("mv .out00000001$glob $nexf");
-            godo ("rm -rf .out*");
-        } elsif ($method eq 'flowforward') {
+            godo ("mv $tmp/00000000$glob $nexf");
+            godo ("rm -rf $tmp/*");
+        } elsif ($method eq 'flowforward' or $method eq 'c') {
             my $fflow = "flow-fwd-".($n)."-".($n+1).".sVflow";
             print "flow from $_ to $next\n";
-            godo ("$xf $_ $next $fflow");
+            godo ("$xf $_ $next $fflow $args");
             print "oneway interpolation\n";
-            godo ("$si forward $_ $fflow .out%1$glob 0 2");
+            godo ("$si forward $_ $fflow $tmp/%1$glob 0 0");
             print "saving center interpolation\n";
-            godo ("mv .out00000001$glob $nexf");
-            godo ("rm -rf .out*");
-
+            godo ("mv $tmp/00000000$glob $nexf");
+            godo ("rm -rf $tmp/*");
         } elsif ($method eq 'average') {
             print "rendering average between $_ and $next\n";
             godo ("convert $_ $next -average -flatten $nexf");
         } elsif ($method eq 'debug') {
             print "debug, skipping conversion\n";
+        } else {
+            die "method no supported: $method";
         }
 
         $o++;
