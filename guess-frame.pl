00e7d3650f6082f04e9827b80d43675cbefe0abb
Patricio Reboratti <darthpolly@gmail.com>
2013-10-09 19:00:05 -0300

    Add guess frame, tool for converting (scene, number) into (frame).
    Signed-off-by: Patricio Reboratti <darthpolly@gmail.com>

---
+++ guess-frame.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $usage = "usage: guess-frame.pl <scene_number> [ <image_number> ... ]\n";
+my $sc_prefix = "scene";
+my $sc_num_len = 4;
+my $fixed_num_len = 6;
+my $scene;
+my $frame;
+
+
+# Helpers
+
+sub zero($$) {
+    my ($val, $cant) = @_;
+    while (length($val) < $cant) {
+        $val = "0".$val;
+    }
+    return $val;
+}
+
+
+# Procedure
+
+$scene = shift(@ARGV);
+
+if (!$scene) {
+    print $usage;
+    exit;
+}
+
+my $dir = $sc_prefix.zero($scene, $sc_num_len)."/";
+
+foreach (@ARGV) {
+    my $file = $dir."fixed-".zero($_, $fixed_num_len)."a.png";
+    if (! -l $file) {
+        print "$file is not a link!\n";
+    } else {
+        while(-l $file) {
+            $file = $dir.readlink($file);
+        }
+        ($frame) = $file =~ m/^$dir[^\d]+([\d]+)[^\d]+$/;
+        print --$frame, "\n";
+    }
+}
